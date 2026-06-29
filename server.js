bb051c845aa0bbddbe217974c5364db0ceeb9f19
amirparsakeshavarzy-netizen <amirparsakeshavarzy@gmail.com>
2026-06-29 15:56:25 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+http.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
