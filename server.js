1f59247440487ce6151c85935c0448c4795c2729
amirparsakeshavarzy-netizen <amirparsakeshavarzy@gmail.com>
2026-06-29 17:06:51 +0330

    Update server.js

--- server.js
+++ server.js
@@ -6,41 +6,19 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
+  console.log("a user connected");
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
-    });
-});
-const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-
-app.use(express.static("public"));
-
-io.on("connection", (socket) => {
-  console.log("user connected");
-
-  // دریافت پیام از یک کاربر
   socket.on("chat message", (msg) => {
-    // فرستادن به همه کاربران
     io.emit("chat message", msg);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+});
 
-http.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
